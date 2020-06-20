46f4a7d921e8cecce61d48a25de499d97d16676c
cmanish049 <cmanish049@gmail.com>
2020-06-20 20:26:54 +0545

    5 adding header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
-  <div class="container" id="app">
-    <router-view />
+  <div class="container">
+    <app-header></app-header>
+
+        <router-view />
+
   </div>
 </template>
-
+<script>
+import Header from './components/Header'
+export default {
+  components: {
+    appHeader: Header
+  }
+}
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,36 @@
 <template>
-  <h1>Header Component</h1>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <router-link to="/" class="navbar-brand">Stock Trader</router-link>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <router-link to="/portfolio" class="nav-item" activeClass="active" tag="li">
+        <a class="nav-link" href="#">Portfolio <span class="sr-only">(current)</span></a>
+      </router-link>
+      <router-link to="/stocks" class="nav-item" activeClass="active" tag="li">
+        <a class="nav-link" href="#">Stocks</a>
+      </router-link>  
+        
+    </ul>
+    <ul class="navbar-nav navbar-right">
+        <li class="nav-item">
+        <a class="nav-link" href="#">End Day</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Save & Load
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Save Data</a>
+          <a class="dropdown-item" href="#">Load Data</a>
+        </div>
+      </li>  
+    </ul>
+  </div>
+</nav>
 </template>
 
 <script>
