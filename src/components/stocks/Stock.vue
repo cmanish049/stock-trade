73e03c57878e348c971e6cd8f91ed1ec916d4cd1
cmanish049 <cmanish049@gmail.com>
2020-06-20 20:48:49 +0545

    7 creating stocks component

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,5 +1,24 @@
 <template>
-  <h1>Stocks - Stock Component</h1>
+  <div class="col-sm-6 col-md-4 mt-2 mb-2">
+    <div class="card border-success">
+    <div class="card-header">
+      NAME
+      <small>PRICE</small>
+    </div>
+    <div class="card-body  text-success">
+      <div class="float-left">
+        <input 
+          type="number"
+          class="form-control"
+          placeholder="Quantity"
+          >
+      </div>
+      <div class="float-right">
+        <button class="btn btn-success">Buy</button>
+      </div>
+    </div>
+</div>
+  </div>
 </template>
 
 <script>
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,10 +1,26 @@
 <template>
-  <h1>Stocks Component</h1>
+  <div class="row">
+    <app-stock v-for="stock in stocks" :key="stock.id"></app-stock>
+  </div>
+
 </template>
 
 <script>
+import Stock from "./Stock";
 export default {
-
+  components: {
+    appStock: Stock,
+  },
+  data() {
+    return {
+      stocks: [
+        {id: 1, name:'MeroBhakaari', price: 110},
+        {id: 2, name:'Spinel Tech', price: 100},
+        {id: 3, name:'Isop Nepal', price: 120},
+        {id: 4, name:'Neptrox', price: 150},
+      ]
+    };
+  },
 }
 </script>
 
